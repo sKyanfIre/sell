297c46038273977327acabc0db9eb647ad965ccf
zzz <911930036@qq.com>
2020-03-01 02:32:35 +0800

    zty->init repo

---
+++ web/src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+*{
+  box-sizing: border-box;
+}
+html, body, #app {
+  width: 100%;
+  height: 100%;
+  margin: 0px;
+  padding: 0px;
+  font-size: 16px;
+}
+ul,li{
+  list-style: none;
+  margin: 0px;
+  padding: 0px;
+}
+.button{
+  background: #79bbff;
+  background-image: -webkit-linear-gradient(top, #79bbff, #378de5);
+  background-image: -moz-linear-gradient(top, #79bbff, #378de5);
+  background-image: -ms-linear-gradient(top, #79bbff, #378de5);
+  background-image: -o-linear-gradient(top, #79bbff, #378de5);
+  background-image: linear-gradient(to bottom, #79bbff, #378de5);
+  -webkit-border-radius: 5%;
+  -moz-border-radius: 5%;
+  border-radius: 5px;
+  text-shadow: 0px 1px 0px #528ecc;
+  -webkit-box-shadow: 3px 4px 0px 0px #1564ad;
+  -moz-box-shadow: 3px 4px 0px 0px #1564ad;
+  box-shadow: 3px 4px 0px 0px #1564ad;
+  font-family: Arial;
+  color: #ffffff;
+  font-size: 16px;
+  padding: 12px 30px 12px 30px;
+  border: solid #337bc4 1px;
+  text-decoration: none;
+}
+
+.button:hover {
+  color: #ffffff;
+  background: #378de5;
+  text-decoration: none;
+}</style>
---
+++ web/src/components/Login.vue
@@ -0,0 +1,135 @@
+<template>
+<div>
+  <div class="login">
+    <div id="logo">
+      <span id="project-name">房产直销系统</span>
+      <img src="../assets/img/login_logo.jpg">
+    </div>
+    <div id="form">
+      <div id="div-form">
+        <ul>
+          <li>
+            <span>账号</span>
+          </li>
+          <li>
+            <input id="input-username" type="text" name="username">
+          </li>
+          <li>
+            <span>密码</span>
+          </li>
+          <li>
+            <input id="input-password" type="password" name="password">
+          </li>
+          <li>
+            <input id="input-remember" type="checkbox" name="remember">
+            <span>记住密码</span>
+          </li>
+          <li>
+            <button id="button-login" class="button">登录</button>
+          </li>
+          <li>
+            <span>还没有账号？</span>
+            <a>创建一个账号</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="footer">
+      <span>Copyright © 售房网 2020 All Rights Reserved </span>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+
+  name: 'Login',
+  data () {
+    return {
+      username: '911930036',
+      password: '123456'
+    }
+  }
+
+}
+
+</script>
+
+<style scoped>
+.login{
+    width: 100vw;
+    height: 100vh;
+    background-image: url("../assets/img/background2.jpg");
+    background-size: cover;
+    background-repeat: no-repeat;
+    font-size: 16px;
+}
+#logo{
+  text-align: center;
+  width: 100vw;
+  /*height: 30vh;*/
+  padding-top: 5vh;
+  padding-bottom: 5vh;
+}
+#logo > span{
+  position: absolute;
+  left: 20px;
+}
+#logo > img{
+  border-radius: 50%;
+  width: 6vw;
+}
+#form{
+  width: 100vw;
+}
+#form > div{
+  background-color: white;
+  border-style: solid;
+  border-width: 1px;
+  border-color: black;
+  border-radius: 1rem;
+  width: 25vw;
+  height: 30vw;
+  margin: 0 auto;
+  padding: 2rem;
+}
+
+#form ul li{
+  width: 100%;
+  margin-bottom: 0.5rem;
+}
+#input-username,#input-password,#button-login{
+  width: 100%;
+  border-radius: 0.25rem;
+  padding: 0.5rem 0.75rem;
+  font-size: 1rem;
+  line-height: 1.25;
+  border: 1px solid rgba(0,0,0,0.15);
+  overflow: visible;
+}
+#button-login{
+  background-color: #007bff;
+  color: #ffffff;
+  border-color: #007bff;
+}
+#project-name{
+  font-family: Arial;
+  font-size: 30px;
+  font-weight: bolder;
+  color: rgb(226, 141, 43);
+}
+.footer{
+  font-family: Arial;
+  color: grey;
+  font-size: 16px;
+  position: absolute;
+  bottom: 4vh;
+  left: 50%;
+  /* transform: translateX(-50%) translateY(-50%); */
+}
+.footer > span{
+  position: relative;
+  left: -50%;
+}
+</style>
